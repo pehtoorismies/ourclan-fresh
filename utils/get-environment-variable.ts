92402edbccd9524eac0b3ee97d3c9ecc473f3e78
pehtoorismies <pehtorismies@gmail.com>
2024-02-13 12:06:40 +0200

    ci: test separate ci

--- utils/get-environment-variable.ts
+++ utils/get-environment-variable.ts
@@ -1,7 +1,3 @@
-import * as mod from 'https://deno.land/std@0.215.0/dotenv/mod.ts'
-
-await mod.load()
-
 export const getEnvironmentVariable = (envVar: string): string => {
   const value = Deno.env.get(envVar)
   if (!value) {
