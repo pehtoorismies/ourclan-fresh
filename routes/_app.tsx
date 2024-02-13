d9864b1cd51613bddfbcd691c4cd19d2b59e574e
pehtoorismies <pehtorismies@gmail.com>
2024-02-13 10:59:32 +0200

    fix: lint

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -26,7 +26,11 @@ export default function App({ Component }: PageProps) {
         />
         <link rel='manifest' href='/site.webmanifest'></link>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
         <link
           href='https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap'
           rel='stylesheet'
