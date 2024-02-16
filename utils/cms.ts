9b45ead3d0b6c4882fd5c6f8b8f290e0984d3be0
pehtoorismies <pehtorismies@gmail.com>
2024-02-16 11:11:31 +0200

    feat: add access token to clien

--- utils/cms.ts
+++ utils/cms.ts
@@ -3,6 +3,7 @@ import { getEnvironmentVariable } from '/utils/get-environment-variable.ts'
 
 export const getClient = () => {
   return createClient(getEnvironmentVariable('PRISMIC_REPO'), {
+    accessToken: getEnvironmentVariable('PRISMIC_ACCESS_TOKEN'),
     fetch,
   })
 }
